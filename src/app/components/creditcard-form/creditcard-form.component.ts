3d07cfa55c6f53352288d6175203ac0328161a86
Marco Andres Silva <marco.silvaponce10@gmail.com>
2025-07-17 23:00:49 -0400

    feat: add anglar material and imports

--- src/app/components/creditcard-form/creditcard-form.component.ts
+++ src/app/components/creditcard-form/creditcard-form.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-creditcard-form',
+  standalone: true,
   imports: [],
   templateUrl: './creditcard-form.component.html',
   styleUrl: './creditcard-form.component.scss'
